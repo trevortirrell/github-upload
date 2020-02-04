727270adf81cdc55a2125502abeea01aff06b67d
Trevor Tirrell <ttirrell@hotmail.com>
2020-02-04 08:29:11 -0500

    Add project files.

---
+++ MeetingBuddy/Views/Counter.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MeetingBuddy.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Counter : ContentPage
+    {
+        private int count = 0;
+        public Counter()
+        {
+            InitializeComponent();
+        }
+
+        private void IncrementCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = count.ToString();
+        }
+    }
+}
